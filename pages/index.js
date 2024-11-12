719af1d3c3710457fafaf58935075b0db4371605
zayn <zayn442@hotmail.com>
2024-11-12 18:10:25 +0000

    Install react-icons

--- pages/index.js
+++ pages/index.js
@@ -9,7 +9,7 @@ export default function Home() {
       <Head>
         <title>Zayn Ul-Haq - Portfolio</title>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
         <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;600&display=swap" rel="stylesheet" />
       </Head>
       
